231ebf5f08e730872bdf5f691c4e32d8841231bb
Matheus Andrade <matheusandrade009@gmail.com>
2022-11-03 10:57:04 -0300

    Feat: framer motion

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,12 +1,26 @@
 import React from "react";
 import { SocialIcon } from "react-social-icons";
+import { motion } from "framer-motion"
 
 type Props = {};
 
 export default function Header({}: Props) {
   return (
-    <header>
-      <div className="flex flex-row items-center">
+    <header className="sticky top-0 p-5 flex items-start justify-between max-w-7xl mx-auto z-20 xl:items-center">
+      <motion.div
+      initial={{
+        x: -500,
+        opacity: 0,
+        scale: 0.5,
+      }} 
+
+      animate={{
+        x: 0,
+        opacity: 1,
+        
+      }}
+      
+      className="flex flex-row items-center">
         <SocialIcon
           url="https://www.facebook.com/profile.php?id=100011316218129"
           fgColor="gray"
@@ -22,15 +36,16 @@ export default function Header({}: Props) {
           fgColor="gray"
           bgColor="transparent"
         />
-      </div>
+      </motion.div>
 
-      <div>
+      <div className="flex flex-row items-center text-gray-300 cursor-pointer">
         <SocialIcon
           className="cursor-pointer"
           network="email"
           fgColor="gray"
           bgColor="transparent"
         />
+        <p className="uppercase hidden md:inline-flex text-sm text-gray-400">Get In Touch</p>
       </div>
     </header>
   );
