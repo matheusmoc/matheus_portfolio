8757c3b036a057d0d6c586b0c5e07fe28caff91e
Matheus Andrade <matheusandrade009@gmail.com>
2022-11-04 11:43:48 -0300

    refactor: Style hero

--- components/BackgroundCircle.tsx
+++ components/BackgroundCircle.tsx
@@ -18,7 +18,7 @@ export default function BackgroundCircle({}: Props) {
         duration: 2.5
     }}
     className='relative flex justify-center items-center'>
-        <div className='absolute border border-[#5a5a5a] rounded-full h-[200px] w-[200px] mt-52 animate-ping'/>
+        <div className='absolute border border-[#333333] rounded-full h-[200px] w-[200px] mt-52 animate-ping'/>
         <div className='absolute border border-[#6e00a5] rounded-full h-[500px] w-[500px] mt-52'/>
         <div className='absolute border border-[purple] rounded-full h-[650px] w-[650px] mt-52 animate-pulse'/>
         <div />
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import { useTypewriter, Cursor } from "react-simple-typewriter";
 import BackgroundCircle from "./BackgroundCircle";
 
@@ -7,21 +8,49 @@ export default function Hero({}: Props) {
   const [text, count] = useTypewriter({
     words: [
       "Hi, my name is Matheus and i love coffe ☕",
-      "How about we do something more modern for your business?",
-      "<FollowMe❤️ />",
+      "Want something modern for your business?",
+      "<FollowMe 😉 />",
     ],
     loop: true,
-    delaySpeed: 1000,
-    typeSpeed: 25,
-    deleteSpeed: 10,
+    delaySpeed: 2000,
+    typeSpeed: 60,
+    deleteSpeed: 20,
   });
   return (
     <div className="h-screen flex flex-col space-y-8 items-center justify-center text-center overflow-hidden">
-        <BackgroundCircle />
-      <h1>
-        <span>{text}</span>
-        <Cursor cursorColor="#6e00a5" cursorStyle="_" />
-      </h1>
+      <BackgroundCircle />
+      <img
+        className="relative rounded-full h-32 w-32 mx-auto object-cover"
+        src="https://avatars.githubusercontent.com/u/73448357?s=400&u=d10689a047976a5b60acb2bff725719c1c22301b&v=4"
+        alt=""
+      />
+      <div className="z-20">
+        <h2 className="font-bold text-sm uppercase text-gray-500  pb-2 tracking-[15px]">
+          Software Engineer
+        </h2>
+        <h1 className="text-5xl lg:text-6xl font-semibold px-10">
+          <span className="mr-3">{text}</span>
+          <Cursor cursorColor="#7858A6" cursorStyle="|" />
+        </h1>
+
+        <div className="pt-52">
+          <Link href="#about">
+            <button className="heroButton">About</button>
+          </Link>
+
+          <Link href="#experience">
+            <button className="heroButton">Experience</button>
+          </Link>
+
+          <Link href="#skills">
+            <button className="heroButton">Skills</button>
+          </Link>
+
+          <Link href="#projects">
+            <button className="heroButton">Projects</button>
+          </Link>
+        </div>
+      </div>
     </div>
   );
 }
