ee72c9bc80b99b3e18948a96aded2c7a3e240085
Matheus Andrade <matheusandrade009@gmail.com>
2022-11-11 00:57:32 -0300

    Fix: Hero words size

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -28,15 +28,15 @@ export default function Hero({}: Props) {
         alt=""
       />
       <div className="z-20">
-        <h2 className="flex justify-center font-bold text-sm uppercase text-gray-500 pb-2 tracking-[10px]">
+        <h2 className="flex justify-center font-bold text-sm uppercase text-gray-500 pb-1 tracking-[10px]">
           Software Developer
         </h2>
-        <h1 className="text-5xl lg:text-6xl font-semibold px-10">
+        <h1 className="text-3xl lg:text-6xl font-semibold px-10">
           <span className="mr-3">{text}</span>
           <Cursor cursorColor="#7858A6" cursorStyle="|" />
         </h1>
 
-        <div className="flex justify-center xl:pt-52 pt-32">
+        <div className="flex justify-center xl:pt-52 pt-28">
           <Link href="#about">
             <button className="heroButton pr-4 pl-4">About</button>
           </Link>
